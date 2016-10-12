c553fe117480b83fc3191a49fa89c5a7c4f4721f
skripka <vitaliy.skripka23@gmail.com>
2016-10-12 17:14:34 +0300

    wazuuuuuuup1

--- react-1/src/js/pages/von.js
+++ react-1/src/js/pages/von.js
@@ -6,7 +6,7 @@ export default class poshelVom extends React.Component {
             <div class="text-center">
                 <h1 class="text-center">Вы кто такие?</h1>
                 <h1 class="text-center">Я вас не звал!</h1>
-                <h1 class="text-center">Идите нахуй отсюда!</h1>
+                <h1 class="text-center">Идите нахуй отсюда!!!</h1>
             </div>
         );
     }
